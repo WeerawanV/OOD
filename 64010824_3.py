c33e45bfe38a91346b42878e980167be5577ff03
wanttosleep <88647945+WeerawanV@users.noreply.github.com>
2022-08-15 22:51:23 +0700

    Delete 64010824_3.py

--- 64010824_3.py
+++
@@ -1,7 +0,0 @@
-print(" *** Summation of each digit ***")
-n = int(input("Enter a positive number : "))
-sum = 0
-while n != 0 :
-        sum += n % 10
-        n //= 10
-print("Summation of each digit = ", sum)
