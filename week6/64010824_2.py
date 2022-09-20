3d4e36e118a9e2c7facaafb305ad74b1dfcb3bc9
wanttosleep <88647945+WeerawanV@users.noreply.github.com>
2022-09-21 00:22:12 +0700

    Update 64010824_2.py

--- week6/64010824_2.py
+++ week6/64010824_2.py
@@ -1,3 +1,14 @@
+''' ให้นักศึกษาเขียนฟังก์ชันที่ทำงานเหมือนกับฟังก์ชัน len() เพื่อหาความยาวของ string และแสดงผลดังตัวอย่าง(print ตัวอักษรตามด้วยเครื่องหมายพิเศษสลับกันคู่คี่)
+
+****ห้ามใช้คำสั่ง len, for, while, do while, split*****
+
+หมายเหตุ ฟังก์ชันต้องมี parameter แค่เพียง 1 ตัว 
+
+def length(txt):     
+    #Code Here
+print("\n",length(input("Enter Input : ")),sep="")
+#ตรง print(เป็นแค่ตัวอย่างสามารถแก้ไขได้) '''
+
 def length(txt):    
     if txt == "": #base case
         print("")
