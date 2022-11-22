ef30ca0182ba3a4dc1449fe2b22c90ed905d4d2b
wanttosleep <88647945+WeerawanV@users.noreply.github.com>
2022-11-22 22:47:32 +0700

    Update 64010824_5.py

--- week9/64010824_5.py
+++ week9/64010824_5.py
@@ -1,3 +1,15 @@
+'''สร้างฟังก์ชันที่นำชุดข้อมูล(list)ของ football clubs ที่มีคุณสมบัติดังนี้ name, wins, loss, draws, scored, conceded และทำการ return list ของ team name 
+โดยเรียงลำดับทีมที่มีคะแนน(total point)มากที่สุด โดยถ้าหากมีทีมที่คะแนนเท่ากัน ให้นำผลต่างของจำนวนประตูที่ทำได้(scored)กับจำนวนประตูที่เสีย(conceded) มาคิด
+
+***ห้ามใช้ Built-in Function ที่เกี่ยวกับ Sort เช่น sort, min, max,ฯลฯ***
+
+[ชนะได้ 3 คะแนน, เสมอได้ 1 คะแนน, แพ้ได้ 0 คะแนน]
+
+ตัวอย่าง
+team = { "name": "Manchester United", "wins": 30, "loss": 3, "draws": 5, "scored": 88, "conceded": 20 }
+Total Points = 3 * wins + 0 * loss + 1 * draws = 3 * 30 + 0 * 3 + 5 * 1 = 95 points
+Goal Difference = scored - conceded = 88 - 20 = 68 '''
+
 def sortFootballClubs(lists):
     sortedLists = []
     for i in lists:
