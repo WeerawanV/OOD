59640d5af7dc8dd146b523c6109dbd3fb1229246
wanttosleep <88647945+WeerawanV@users.noreply.github.com>
2022-11-22 22:43:48 +0700

    Update 64010824_1.py

--- week9/64010824_1.py
+++ week9/64010824_1.py
@@ -1,3 +1,7 @@
+'''รับ input เป็น list แล้วแสดงขั้นตอนของ bubble sort ตามตัวอย่าง
+
+***ห้ามใช้ Built-in Function ที่เกี่ยวกับ Sort เช่น .sort ให้น้องเขียนฟังก์ชัน Sort เอง และห้าม Import*** '''
+
 inp = [int(x) for x in input("Enter Input : ").split()]
 
 for i in range(len(inp)-1):
